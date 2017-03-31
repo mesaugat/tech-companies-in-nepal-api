5556af3fe78ddc0eeadd9bb622af0b3f7694c2eb
Saugat Acharya <mesaugat@gmail.com>
2017-03-31 21:41:19 +0545

    Serve favicon

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const express = require('express');
 const winston = require('winston');
 const routes = require('./routes');
+const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const jsonResponse = require('./middleware/response');
 const errorHandler = require('./middleware/error-handler');
@@ -10,6 +12,8 @@ const errorHandler = require('./middleware/error-handler');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(helmet());
 app.use(morgan('tiny'));
